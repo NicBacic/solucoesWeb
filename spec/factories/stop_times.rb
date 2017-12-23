0ee746728942101c002260d8ebf601ed52b488e9
NickBacic <nicbacic@gmail.com>
2017-12-23 19:03:17 -0200

    Just a try in css, but I am completely trash at this

--- spec/factories/stop_times.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :stop_time do
-    trip_id "MyString"
-    arrival_time "2017-12-14 14:47:38"
-    departure_time "2017-12-14 14:47:38"
-    stop_id 1
-    stop_sequence 1
-  end
-end
